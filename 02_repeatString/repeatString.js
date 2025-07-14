64f588c8a06105962a0a5d76b4388f0643fa631d
Daniel Herrero Hernando <danielherrerohernando@gmail.com>
2025-07-14 21:28:08 +0200

    example of push

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (text, numTimes) {
+    if (numTimes < 0) {
+        return 'ERROR'
+    }
+    if (typeof text !== 'string') {
+        return 'You need to pass a string to repeat'
+    }
+    return text.repeat(numTimes)
 };
 
 // Do not edit below this line
